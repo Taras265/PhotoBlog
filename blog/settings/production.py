a12e64c18f100563301db6e8894183e7e702278d
Taras265 <70756830+Taras265@users.noreply.github.com>
2021-11-25 21:46:20 +0200

    Update production.py

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -31,7 +31,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['134.209.204.45']
 
 
 # Application definition
